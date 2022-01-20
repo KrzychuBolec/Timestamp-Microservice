a802f49507c2646620dd47ec453394c863b98910
zagorny95 <krzysztof.podworny@wp.pl>
2022-01-20 05:50:46 +0100

    V 1.0.8

--- functions.js
+++ functions.js
@@ -1,23 +1,21 @@
 let formatDate = (date) => {
-  let regex = /(^[^0]\d{3}-(0[\d]|1[12])-([0-2][0-9]|3[01])|^\d*\d$)/g; //regex to check valid date format
-
-  let dateValues = [];
-
   if (date != undefined) {
-    if (regex.test(date)) {
-      if (date.includes("-")) {
-        dateValues = date.split("-");
-        let newDate = new Date(dateValues[0], dateValues[1] - 1, dateValues[2]);
-        return {
-          unix: newDate.getTime(),
-          utc: newDate.toUTCString(),
-        };
+    if (isNaN(date)) {
+      let dateObject = new Date(date);
+      if (isNaN(dateObject.getTime())) {
+        return { error: "Invalid Date" };
       } else {
-        let newDate = new Date(parseInt(date));
-        return { unix: parseInt(date), utc: newDate.toUTCString() };
+        return {
+          unix: dateObject.getTime(),
+          utc: dateObject.toUTCString(),
+        };
       }
     } else {
-      return { error: "Invalid Date" };
+      let dateObject = new Date(parseInt(date));
+      return {
+        unix: dateObject.getTime(),
+        utc: dateObject.toUTCString(),
+      };
     }
   } else {
     return {
