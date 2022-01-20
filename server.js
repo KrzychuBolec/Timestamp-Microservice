50c472a7d8fc066f84d86cb3f395bfa63cefb887
zagorny95 <krzysztof.podworny@wp.pl>
2022-01-20 03:57:55 +0100

    V 1.0.1

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 var express = require(`express`);
+const res = require("express/lib/response");
 var app = express();
 var handlers = require("./handlers.js");
 
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", handlers.setHomepage);
+app.get("/", () => {
+  try {
+    handlers.setHomepage;
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 app.get("/api/:date", handlers.queryHandle);
 
-app.listen(5000, () => {
+app.listen(1988, () => {
   console.log("Server is listening on port 5000");
 });
