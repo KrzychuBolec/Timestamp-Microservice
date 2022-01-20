e54b15aaa0d98c34a1449ea2638a8043300aad93
zagorny95 <krzysztof.podworny@wp.pl>
2022-01-20 04:22:29 +0100

    V 1.0.4

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", handlers.setHomepage);
 
-app.get("/api/:date", handlers.queryHandle);
+app.get("/api/:date?", handlers.queryHandle);
 
 const listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
